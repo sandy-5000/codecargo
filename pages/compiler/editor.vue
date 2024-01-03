24c8f87adf0e7c4d729bef32ec95cfd2e2f69bee
Sandeep <bsandeep@corp.untd.com>
2024-01-03 15:56:26 +0530

    added alert message

--- pages/compiler/editor.vue
+++ pages/compiler/editor.vue
@@ -1,55 +1,98 @@
 <template>
   <NuxtLayout :name="layout" page="editor" header="Home" title="Editor">
-    <compiler-language v-if="modal" :selectLanguage="(id, language) => {
-      selected = language
-      closeModal()
-      swiftLanguage(id)
-    }"></compiler-language>
+    <compiler-language
+      v-if="modal"
+      :selectLanguage="
+        (id, language) => {
+          selected = language
+          closeModal()
+          swiftLanguage(id)
+        }
+      "
+    ></compiler-language>
+    <message-alert :message="message" @close="(x) => (message = x)" />
     <div :class="{ 'editor w-full': true, 'blur-md': blur }">
       <div class="py-4 px-3 flex justify-end md:justify-start">
         <ui-button-secondary @click="openModal">
           <span class="select-btn">{{ selected }}</span>
         </ui-button-secondary>
       </div>
-      <div class="w-full px-3 pb-5" style="height: calc(100vh - 50px);">
+      <div class="w-full px-3 pb-5" style="height: calc(100vh - 50px)">
         <div class="h-full">
-          <iframe title="compiler" id="oc-editor" class="h-full w-full rounded-lg"
-            src="https://onecompiler.com/embed?hideTitle=true&codeChangeEvent=true&listenToEvents=true&hideNew=true&hideLanguageSelection=true"></iframe>
+          <iframe
+            title="compiler"
+            id="oc-editor"
+            class="h-full w-full rounded-lg"
+            src="https://onecompiler.com/embed?hideTitle=true&codeChangeEvent=true&listenToEvents=true&hideNew=true&hideLanguageSelection=true"
+          ></iframe>
         </div>
       </div>
       <div v-if="session._id">
-        <div class="pb-5 px-5 flex flex-col-reverse md:flex-row justify-between">
+        <div
+          class="pb-5 px-5 flex flex-col-reverse md:flex-row justify-between"
+        >
           <div class="px-4 mt-3 md:mt-0">
             <ui-input-label value="Channel ID" />
             <div class="relative">
-              <div v-if="!!channelId" class="absolute right-0 top-0 h-full aspect-square p-1">
-                <div @click="copyChannel(channelId)"
-                  class="h-full bg-gray-800 cursor-pointer border-2 border-indigo-400 rounded-md a-center p-1">
-                  <font-awesome-icon style="color: #f1f5f9" class="text-sm" icon="fa-solid fa-clone" />
+              <div
+                v-if="!!channelId"
+                class="absolute right-0 top-0 h-full aspect-square p-1"
+              >
+                <div
+                  @click="copyChannel(channelId)"
+                  class="h-full bg-gray-800 cursor-pointer border-2 border-indigo-400 rounded-md a-center p-1"
+                >
+                  <font-awesome-icon
+                    style="color: #f1f5f9"
+                    class="text-sm"
+                    icon="fa-solid fa-clone"
+                  />
                 </div>
               </div>
-              <ui-text-input className="block mt-1 w-full" :value="inputChannelId"
-                @update:value="(x) => inputChannelId = x" :disabled="!!channelId" />
+              <ui-text-input
+                className="block mt-1 w-full"
+                :value="inputChannelId"
+                @update:value="(x) => (inputChannelId = x)"
+                :disabled="!!channelId"
+              />
             </div>
-            <span v-if="copied" class="text-gray-200 text-xs">Copied to clipboard.</span>
             <div class="mt-1 flex justify-end md:justify-start">
-              <ui-button-primary @click="joinChannel()" v-if="!loading.join && !channelId" class="mt-1">
+              <ui-button-primary
+                @click="joinChannel()"
+                v-if="!loading.join && !channelId"
+                class="mt-1"
+              >
                 Join Channel
               </ui-button-primary>
-              <ui-button-danger @click="leaveChannel()" v-if="!loading.join && !!channelId" class="mt-1">
+              <ui-button-danger
+                @click="leaveChannel()"
+                v-if="!loading.join && !!channelId"
+                class="mt-1"
+              >
                 Leave Channel
               </ui-button-danger>
             </div>
           </div>
           <div class="flex justify-end md:block">
-            <ui-switch id="sync_code" :value="codeSync" @update:checked="(x) => codeSync = x"
-              prompt="Synchronize Code"></ui-switch>
+            <ui-switch
+              id="sync_code"
+              :value="codeSync"
+              @update:checked="(x) => (codeSync = x)"
+              prompt="Synchronize Code"
+            ></ui-switch>
           </div>
         </div>
         <div class="a-center my-5">
-          <Button @click="createChannel()" :disabled="loading.create || !!channelId"
-            class="border-2 border-indigo-600 rounded-lg z-[1] py-4 px-10 bg-gray-800 text-slate-100 a-center">
-            <font-awesome-icon fill="#f1f5f9" class="mr-3" icon="fa-solid fa-plus" />
+          <Button
+            @click="createChannel()"
+            :disabled="loading.create || !!channelId"
+            class="border-2 border-indigo-600 rounded-lg z-[1] py-4 px-10 bg-gray-800 text-slate-100 a-center"
+          >
+            <font-awesome-icon
+              fill="#f1f5f9"
+              class="mr-3"
+              icon="fa-solid fa-plus"
+            />
             <span class="font-normal">Create Channel</span>
           </Button>
         </div>
@@ -77,7 +120,7 @@ const codeSync = useState('codeSync', () => false)
 const blur = useState('blur', () => false)
 const channelId = useState('channelId', () => '')
 const inputChannelId = useState('inputChannelId', () => '')
-const copied = useState('copied', () => false)
+const message = useState('message', () => '')
 
 const loading = useState('loading', () => {
   return {
@@ -102,14 +145,22 @@ const swiftLanguage = (lang) => {
   if (data) {
     x = JSON.parse(data)
   }
-  editor.contentWindow?.postMessage({
-    eventType: 'populateCode',
-    language: x.language,
-    files: x.files,
-  }, "*")
+  editor.contentWindow?.postMessage(
+    {
+      eventType: 'populateCode',
+      language: x.language,
+      files: x.files,
+    },
+    '*'
+  )
   const channel_id = localStorage.getItem('channel_id')
   if (channel_id && codeSync.value) {
-    socket.emit('code-change', { channel_id, user_id, language: x.language, files: x.files })
+    socket.emit('code-change', {
+      channel_id,
+      user_id,
+      language: x.language,
+      files: x.files,
+    })
   }
 }
 
@@ -121,11 +172,14 @@ socket.on('multicast', ({ sender_id, language, files }) => {
   if (!editor) {
     editor = document.getElementById('oc-editor')
   }
-  editor.contentWindow?.postMessage({
-    eventType: 'populateCode',
-    language: language,
-    files: files,
-  }, "*")
+  editor.contentWindow?.postMessage(
+    {
+      eventType: 'populateCode',
+      language: language,
+      files: files,
+    },
+    '*'
+  )
 })
 
 const handleData = (e) => {
@@ -139,15 +193,13 @@ const handleData = (e) => {
   }
 }
 
-const copyChannel = (channel_id) => {
-  navigator.clipboard.writeText(channel_id)
+const copyChannel = (channel_id, msg) => {
+  navigator.clipboard
+    .writeText(channel_id)
     .then(() => {
-      copied.value = true
-      setTimeout(() => {
-        copied.value = false
-      }, 1000)
+      message.value = msg || 'Copied to clipboard.'
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err)
     })
 }
@@ -161,8 +213,8 @@ const createChannel = async () => {
     const response = await $fetch('/api/channel/create', {
       method: 'POST',
       body: {
-        _id: 'sandy-blaze'
-      }
+        _id: 'sandy-blaze',
+      },
     })
     if (response.channel_id) {
       if (!validate(response.channel_id)) {
@@ -175,7 +227,7 @@ const createChannel = async () => {
       localStorage.setItem('channel_id', response.channel_id)
       channelId.value = response.channel_id
       inputChannelId.value = response.channel_id
-      copyChannel(response.channel_id)
+      copyChannel(response.channel_id, 'Channel ID Created, Copied to clipboard.')
     }
   } catch (e) {
     console.log(e)
@@ -190,7 +242,7 @@ const joinChannel = async () => {
     return
   }
   if (!validate(channel_id)) {
-    alert('Invalid channel')
+    message.value = 'Invalid channel'
     return
   }
   loading.value.join = true
@@ -200,7 +252,7 @@ const joinChannel = async () => {
       body: {
         _id: user_id,
         channel_id: channel_id,
-      }
+      },
     })
     if (response.channel_id) {
       if (!validate(response.channel_id)) {
@@ -242,6 +294,7 @@ const runOnMount = () => {
     create: false,
     join: false,
   }
+  message.value = ''
   codeSync.value = false
   user_id = getUserId()
   editor = document.getElementById('oc-editor')
